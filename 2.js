46ca7df1883c29931521e7cf388571eec999fa69
Tomas22 <gd139@139.com>
2023-01-14 22:02:20 +0800

    2.js

---
+++ 2.js
@@ -0,0 +1,5 @@
+//const { Configuration, OpenAIApi } = require("openai")
+//const configuration = new Configuration({
+// //url:'https://api.openai.com/v1/images/generations',
+// apiKey: `${config.API_KEY}`
+// });
